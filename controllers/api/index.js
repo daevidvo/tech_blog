2cde0bdda15e161e990542a02507e5014b92f8fe
daevidvo <itsdavidvo@gmail.com>
2023-05-07 14:46:05 -0700

    added login and logout logic along with routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userRoutes = require('./userRoutes.js')
+const commentRoutes = require('./commentRoutes.js')
 
 router.use('/users', userRoutes)
+router.use('/comments', commentRoutes)
 
 module.exports = router
